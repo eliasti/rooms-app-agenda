be81cc02e0ca0650b28bce9967e5c66ea0e16695
Elias Aquino <38292592+eliasti@users.noreply.github.com>
2021-04-27 18:40:57 -0300

    Update settings.py

--- rooms/settings.py
+++ rooms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_75&lj@==a@#(hlf)hp8-u=v&oqlr1fbf+xjxo3!!q#lbx1m+%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'https://rooms-agenda-app.herokuapp.com/', 'http://rooms-agenda-app.herokuapp.com/', 'http://rooms-agenda-app.herokuapp.com, 'https://rooms-agenda-app.herokuapp.com', 'rooms-agenda-app.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*', 'https://rooms-agenda-app.herokuapp.com/', 'http://rooms-agenda-app.herokuapp.com/', 'http://rooms-agenda-app.herokuapp.com, '127.0.0.1']
 INTERNAL_IPS = ['localhost', '127.0.0.1']
 
 # Application definition
